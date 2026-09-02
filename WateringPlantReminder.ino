a7d23d4a91045b28658bd429915a5a384f28ba81
Wildbush <75509166+Wildbush76@users.noreply.github.com>
2026-09-02 10:42:27 -0500

    Updating to work with touch sensor

--- WateringPlantReminder.ino
+++ WateringPlantReminder.ino
@@ -2,40 +2,34 @@
 #include <BLEDevice.h>
 #include <BLEUtils.h>
 #include <BLEServer.h>
-#include <Adafruit_NeoPixel.h>
 
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 
 #define SERVICE_UUID "2db9dd7d-1637-47db-96d4-495484ed45e7"
 #define CHARACTERISTIC_UUID "38072c05-608d-441e-987e-69ee78d4a58c"
-#define MOISTURE_SENSOR A0
-#define MOISTURE_SENSOR_POWER A1
+#define MOISTURE_SENSOR D0
 
 //#define DEBUG
 
 
 //constants
-const uint32_t ERROR_COLOR = 0xFF0000;
-const uint32_t OK_COLOR = 0x00FF00;
-const uint32_t SLEEP_COLOR = 0x87CEEB;
 const uint8_t SENSOR_WAKE_TIME = (uint8_t)1000;             //milliseconds
-const uint64_t READING_INTERVAL = (uint64_t)(1000000 * 300);  //in microseconds
+const uint64_t READING_INTERVAL = (uint64_t)(1000000 * 3600);  //in microseconds
 const uint32_t BLE_BROADCAST_TIMEOUT = 1000 * 30;           //milliseconds
 
 //variables
 BLEServer *server = nullptr;
 BLEService *service = nullptr;
-Adafruit_NeoPixel status_led(1, PIN_NEOPIXEL, NEO_GRB + NEO_KHZ800);
 bool active_connection = false;
 
 
 //functions
 void startSleep();
 bool initBLE();
-void initMoistureSensor();
 uint16_t readMoistureSensor();
 void StartBLEBroadcast();
+void error();
 
 
 
@@ -48,21 +42,15 @@ class BLECallbacks : public BLEServerCallbacks {
   }
 };
 
-void set_status_led(uint32_t color) {
-#ifdef DEBUG
-  status_led.setPixelColor(0, color);
-  status_led.show();
-#endif
-}
-
-void initMoistureSensor() {
-  pinMode(MOISTURE_SENSOR_POWER, OUTPUT);
-  digitalWrite(MOISTURE_SENSOR_POWER, HIGH);
-  delay(SENSOR_WAKE_TIME);
+void error() {
+  digitalWrite(LED_BUILTIN,LOW);
+  delay(5000);
+  //trigger sleep
+  startSleep();
 }
 
 uint16_t readMoistureSensor() {
-  return analogRead(MOISTURE_SENSOR);
+  return touchRead(MOISTURE_SENSOR);
 }
 
 bool initBLE() {
@@ -92,22 +80,14 @@ void startSleep() {
 
 
 void setup() {
-
-  pinMode(NEOPIXEL_POWER, OUTPUT);
-#ifdef DEBUG
-  digitalWrite(NEOPIXEL_POWER, HIGH);  //enable light for showing status
-  status_led.clear();
-  status_led.setBrightness(127);  //set half bright
-#else
-  digitalWrite(NEOPIXEL_POWER, LOW);
-#endif
+  pinMode(LED_BUILTIN, OUTPUT);
 
   if (!initBLE()) {
-    set_status_led(ERROR_COLOR);
+    error();
     return;
   }
-  initMoistureSensor();
-  set_status_led(OK_COLOR);
+
+
 
   uint16_t value = readMoistureSensor();
   BLECharacteristic *moisture =
@@ -118,6 +98,8 @@ void setup() {
   delay(BLE_BROADCAST_TIMEOUT);
   if (!active_connection)
     startSleep();
+
+
 }
 
 void loop() {
